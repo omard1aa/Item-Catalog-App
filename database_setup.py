4b69252aad546d0e3840dd931e6b5335f4e64e2a
odian <omardiaa27@gmail.com>
2019-05-14 04:40:13 +0200

    with postgresql

--- database_setup.py
+++ database_setup.py
@@ -1,29 +1,19 @@
-from __builtin__ import property
-
 from sqlalchemy import Integer, String, ForeignKey, Column
 from sqlalchemy import create_engine
 from sqlalchemy.ext import declarative
 from sqlalchemy.orm import sessionmaker, relationship
 from passlib.apps import custom_app_context as pwd_context
 Base = declarative.declarative_base()
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
+Base.metadata.bind = engine
 
-class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    username = Column(String(32))
-    password = Column(String(64))
 
-    def hash_password(self, password_to_be_hashed):
-        self.password = pwd_context.encrypt(password_to_be_hashed)
-
-    def verify_password(self, password_to_be_verified):
-        return pwd_context.verify(password_to_be_verified, self.password)
 
 class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
-    title = Column(String(64), primary_key=True)
-    
+    title = Column(String(64))
+
     @property
     def serialize(self):
         return {
@@ -36,12 +26,8 @@ class Item(Base):
     __tablename__ = 'item'
     id = Column(Integer, primary_key=True)
     name = Column(String(64))
-    category = Column(String(64), ForeignKey('category.title'))
-    category_rel = relationship(Category)
-#    category_id = Column(Integer, ForeignKey('category.id'))
- #   categoryid_rel = relationship(Category, foreign_keys=[category_id])
-
     description = Column(String(64))
+    category = Column(String(64))
 
     @property
     def serialize(self):
@@ -53,5 +39,18 @@ class Item(Base):
         }
 
 
-engine = create_engine('sqlite:///cat-app.db')
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32))
+    password = Column(String(64))
+
+    def hash_password(self, password_to_be_hashed):
+        self.password = pwd_context.encrypt(password_to_be_hashed)
+
+    def verify_password(self, password_to_be_verified):
+        return pwd_context.verify(password_to_be_verified, self.password)
+# engine = create_engine('sqlite:///catalog-app.db')
+
+
 Base.metadata.create_all(engine)
--- db_creation.py
+++ db_creation.py
@@ -1,13 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.ext import declarative
 from database_setup import Category, Base, Item, User
 
 
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
-engine = create_engine('sqlite:///cat-app.db')
+Base = declarative.declarative_base()
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
 Base.metadata.bind = engine
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 # A DBSession() instance establishes all conversations with the database
 # and represents a "staging zone" for all the objects loaded into the
 # database session object. Any change made against the objects in the
@@ -15,51 +19,50 @@ Base.metadata.bind = engine
 # session.commit(). If you're not happy about the changes, you can
 # revert all of them back to the last commit by calling
 # session.rollback()
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+
 
 latest_items = []
 # Categories
-category = Category(title="Soccer", id=1)
+#category1 = Category(title="Soccer", id=10)
 
-session.add(category)
+#session.add(category1)
 
-category = Category(title="Basketball", id=2)
+# category = Category(title="Basketball", id=2)
 
-session.add(category)
+# session.add(category)
 
-category = Category(title="Baseball", id=3)
+# category = Category(title="Baseball", id=3)
 
-session.add(category)
+# session.add(category)
 
-category = Category(title="Frisbee", id=4)
+# category = Category(title="Frisbee", id=4)
 
-session.add(category)
+# session.add(category)
 
-category = Category(title="Snowboarding", id=5)
+# category = Category(title="Snowboarding", id=5)
 
-session.add(category)
+# session.add(category)
 
-category = Category(title="Rock Climbing", id=6)
+# category = Category(title="Rock Climbing", id=6)
 
-session.add(category)
+# session.add(category)
 
-category = Category(title="Foosball", id=7)
+# category = Category(title="Foosball", id=7)
 
-session.add(category)
+# session.add(category)
 
-category = Category(title="Skating", id=8)
+# category = Category(title="Skating", id=8)
 
-session.add(category)
+# session.add(category)
 
-category = Category(title="Hockey", id=9)
+# category = Category(title="Hockey", id=9)
 
-session.add(category)
+# session.add(category)
 
-session.commit()
-print "Categories added!"
+# session.commit()
+# print "Categories added!"
 
-# Category items
+# # # Category items
 item = Item(name='Soccer Cleats', description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', category='Soccer')
 session.add(item)
 
@@ -119,4 +122,3 @@ session.add(item)
 
 session.commit()
 print "Category Items added!"
-
